3202d29b4b1b998e6e1b97c8ad877d05547a11f7
limaarthur <artglima.dev@gmail.com>
2025-08-25 20:46:03 -0300

    displaying elements with scrollreveal

--- 01-doctorcare/main.js
+++ 01-doctorcare/main.js
@@ -29,4 +29,10 @@ ScrollReveal({
   origin: 'top',
   distance: '30px',
   duration: 700,
-}).reveal('#home');
+}).reveal(`
+  #home, 
+  #home img, 
+  #home .stats, 
+  #services,
+  #services header,
+  #services .card`)
